115768e6da5a91c3d8cd097aa5d0a03941c7c8cf
banzhengzheng.alex <banzhengzheng.alex@bytedance.com>
2020-11-12 10:22:20 +0800

    feat: 修改个人页逻辑

--- lib/business/fragment/profile/Profile.dart
+++ lib/business/fragment/profile/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github/github.dart';
 import 'package:gitters/application.dart';
 import 'package:gitters/framework/constants/language/Localizations.dart';
 import 'package:gitters/framework/router/RouterConfig.dart';
@@ -11,6 +12,7 @@ class Profile extends StatefulWidget {
 
 class ProfileState extends State<Profile> {
   var flag = false;
+  CurrentUser user;
   void changeLocale() {
     if (flag) {
       i18nWidgetStateKey.currentState.changeLanguage(const Locale('zh', "CH"));
@@ -36,15 +38,45 @@ class ProfileState extends State<Profile> {
               })
         ],
       ),
-      body: Center(
-        child: RaisedButton(
-            onPressed: () =>
-                Application.router.navigateTo(context, RouterList.Login.value),
-            child: Text(
-              "GO LOGIN!",
-              style: TextStyle(color: Colors.blue),
-            )),
-      ),
+      body: FutureBuilder<CurrentUser>(
+          future: Application.github.users.getCurrentUser(),
+          builder: (BuildContext context, AsyncSnapshot<CurrentUser> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active ||
+                snapshot.connectionState == ConnectionState.waiting) {
+              return new Center(
+                child: new CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              } else if (snapshot.hasData) {
+                user = snapshot.data;
+                print(user.avatarUrl.toString());
+                return Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.network(user.avatarUrl ?? 'https://wx1.sinaimg.cn/mw690/002VTQLVgy1gklq3jwbraj61zo2niqv602.jpg', width: 60, height: 60,),
+                          Column(
+                            children: [
+                              Text(user.name ?? 'default name'),
+                              Text(user.email ?? 'default email'),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                );
+              }
+            }
+            //请求未完成时弹出loading
+            return CircularProgressIndicator();
+          }),
     );
   }
 }
