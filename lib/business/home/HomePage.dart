e9a6c349b4b5bdc18d58d79be32e21d397196ff9
ban-z <banzhengzheng.alex@bytedance.com>
2020-12-05 19:30:26 +0800

    fix: 修复国际化功能

--- lib/business/home/HomePage.dart
+++ lib/business/home/HomePage.dart
@@ -4,6 +4,8 @@ import 'package:gitters/business/fragment/marketplace/Marketplace.dart';
 import 'package:gitters/business/fragment/profile/Profile.dart';
 import 'package:gitters/business/fragment/search/SearchPage.dart';
 import 'package:gitters/framework/global/provider/BaseModel.dart';
+import 'package:gitters/framework/utils/I18n.dart';
+import 'package:gitters/main.dart';
 import 'package:provider/provider.dart';
 import 'HomeSetting.dart';
 
@@ -27,28 +29,22 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // body: _buildBody(),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _bodyList,
+    return I18nWidget(
+      key: i18nWidgetStateKey,
+      child: Scaffold(
+        // body: _buildBody(),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _bodyList,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: BottomNavigationSetting.Indexs,
+          currentIndex: _selectedIndex,
+          // selectedItemColor: context.watch<BaseModel>().themeData.primaryColor,
+          selectedItemColor: context.watch<BaseModel>().themeData.primaryColor,
+          onTap: _onItemTapped,
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: BottomNavigationSetting.Indexs,
-        currentIndex: _selectedIndex,
-        // selectedItemColor: context.watch<BaseModel>().themeData.primaryColor,
-        selectedItemColor: context.watch<BaseModel>().themeData.primaryColor,
-        onTap: _onItemTapped,
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     print("click the change theme button!");
-      //     context
-      //         .read<BaseModel>()
-      //         .updateThemeData(ThemeData(primarySwatch: Colors.cyan));
-      //   },
-      //   child: Icon(Icons.change_history),
-      // ),
     );
   }
 
