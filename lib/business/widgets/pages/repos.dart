94c2dd61cedc8630c1ff7db5fec0beffed52ff74
ban-z <banzhengzheng.alex@bytedance.com>
2021-03-04 16:27:44 +0800

    feat: 优化个人仓库页的展示，加入Follow功能

--- lib/business/widgets/pages/repos.dart
+++ lib/business/widgets/pages/repos.dart
@@ -4,7 +4,17 @@ import 'package:gitters/application.dart';
 import 'package:gitters/business/widgets/repository.dart';
 import 'package:gitters/business/widgets/toast.dart';
 import 'package:gitters/framework/global/constants/language/Localizations.dart';
+import 'package:gitters/framework/global/provider/BaseModel.dart';
 import 'package:gitters/framework/utils/utils.dart';
+import 'package:provider/provider.dart';
+
+class StarConfig {
+  bool isStar;
+  String starHint;
+  IconData starIcon;
+
+  StarConfig(this.isStar, this.starHint, this.starIcon);
+}
 
 class FollowingRepos extends StatefulWidget {
   String user;
@@ -18,13 +28,55 @@ class FollowingRepos extends StatefulWidget {
 }
 
 class _FollowingReposState extends State<FollowingRepos> {
+  User curUser;
+  StarConfig starConfig = StarConfig(false, '--', Icons.star_border);
+  Future userReposInfoFuture;
+
+  String following;
+  String followers;
+
   String getPageTitle(BuildContext context) {
     return widget.title ??
         (GittersLocalizations.of(context).FollowingUser ?? 'Following');
   }
 
+  Future getUserReposInfo() {
+    return gitHubClient.repositories.listUserRepositories(widget.user).toList();
+  }
+
+  @override
+  void initState() {
+    userReposInfoFuture = getUserReposInfo();
+
+    gitHubClient.users.getUser(widget.user).then((user) {
+      setState(() {
+        curUser = user;
+      });
+    }).catchError((err) {
+      setState(() {
+        curUser = null;
+      });
+    });
+
+    gitHubClient.users.isFollowingUser(widget.user).then((res) {
+      starConfig.isStar = res;
+    }).catchError((err) {
+      starConfig.isStar = false;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (starConfig.isStar) {
+      starConfig.starHint =
+          GittersLocalizations.of(context).UnStarButton.toString();
+      starConfig.starIcon = Icons.star;
+    } else {
+      starConfig.starHint =
+          GittersLocalizations.of(context).StarButton.toString();
+      starConfig.starIcon = Icons.star_border;
+    }
     return Scaffold(
         body: NestedScrollView(
             headerSliverBuilder:
@@ -41,13 +93,124 @@ class _FollowingReposState extends State<FollowingRepos> {
                       fit: BoxFit.fill,
                     ),
                   ),
-                )
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.all(0.0),
+                  sliver: new SliverGrid(
+                    //Grid
+                    gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, //Grid按两列显示
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 4.0,
+                    ),
+                    delegate: new SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        following = curUser?.followingCount?.toString() ?? '--';
+                        followers = curUser?.followersCount?.toString() ?? '--';
+
+                        //创建子widget
+                        switch (index) {
+                          case 0:
+                            return new Container(
+                              alignment: Alignment.center,
+                              color: context
+                                  .read<BaseModel>()
+                                  .themeData
+                                  .backgroundColor,
+                              child: new Text('Following: ${following}'),
+                            );
+                            break;
+                          case 1:
+                            return new Container(
+                              alignment: Alignment.center,
+                              color: context
+                                  .read<BaseModel>()
+                                  .themeData
+                                  .backgroundColor,
+                              child: new Text('Followers: ${followers}'),
+                            );
+                            break;
+                          case 2:
+                            return new Container(
+                              alignment: Alignment.center,
+                              color: context
+                                  .read<BaseModel>()
+                                  .themeData
+                                  .backgroundColor,
+                              child: FlatButton(
+                                clipBehavior: Clip.hardEdge,
+                                onPressed: () {
+                                  if (starConfig.isStar) {
+                                    gitHubClient.users
+                                        .unfollowUser(widget.user)
+                                        .then((res) {
+                                      setState(() {
+                                        starConfig = StarConfig(
+                                            false,
+                                            GittersLocalizations.of(context)
+                                                .StarButton
+                                                .toString(),
+                                            Icons.star_border);
+                                        // followers =
+                                        //     (curUser?.followersCount - 1)
+                                        //             ?.toString() ??
+                                        //         '--';
+                                        print('');
+                                      });
+                                    }).catchError((err) {});
+                                  } else {
+                                    gitHubClient.users
+                                        .followUser(widget.user)
+                                        .then((res) {
+                                      setState(() {
+                                        starConfig = StarConfig(
+                                            true,
+                                            GittersLocalizations.of(context)
+                                                .UnStarButton
+                                                .toString(),
+                                            Icons.star);
+                                        // followers =
+                                        //     (curUser?.followersCount + 1)
+                                        //             ?.toString() ??
+                                        //         '--';
+                                        print('');
+                                      });
+                                    }).catchError((err) {});
+                                  }
+                                },
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(starConfig.starIcon),
+                                    Text(starConfig.starHint)
+                                  ],
+                                ),
+                              ),
+                            );
+                            break;
+                          case 3:
+                            return new Container(
+                              alignment: Alignment.center,
+                              color: context
+                                  .read<BaseModel>()
+                                  .themeData
+                                  .backgroundColor,
+                              child: new Text(''),
+                            );
+                            break;
+                          default:
+                            return Container();
+                        }
+                      },
+                      childCount: 4,
+                    ),
+                  ),
+                ),
               ];
             },
             body: FutureBuilder<List<Repository>>(
-                future: gitHubClient.repositories
-                    .listUserRepositories(widget.user)
-                    .toList(),
+                future: userReposInfoFuture,
                 builder: (BuildContext context,
                     AsyncSnapshot<List<Repository>> snapshot) {
                   if (snapshot.connectionState == ConnectionState.active ||
